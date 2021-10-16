c20091d1b3794397188d6d2acbc59a956a092f8e
babelman20 <babelman@purdue.edu>
2021-10-16 14:34:16 -0400

    please stop when killed

--- ros/src/utils/src/ESC_Status.py
+++ ros/src/utils/src/ESC_Status.py
@@ -10,29 +10,31 @@ from shared_msgs.msg import esc_test_data_msg
 lastEsc = [0] * 3
 
 def sendEscData(can_bus):
-    pub = rospy.Publisher('ESCdata', esc_test_data_msg, queue_size=10) # Modify queue size based on rate
+    pub = rospy.Publisher('rov/ESCdata', esc_test_data_msg, queue_size=10) # Modify queue size based on rate
     rospy.init_node('ESC_Logger', anonymous=True)
     rate = rospy.Rate(1) # Figure out what rate to use
     
     while not rospy.is_shutdown():
         for can_rx in can_bus:
-            if not rospy.is_shutdown():
-                can_id = can_rx.arbitration_id
+            if rospy.is_shutdown():
+                break
+            can_id = can_rx.arbitration_id
+            if (0x301 <= can_id <= 0x303): 
+                lastEsc_ = lastEsc[can_id - 0x301]
+                lastEsc[can_id - 0x301] = (lastEsc_ + 1) % 4
+                data = list(can_rx.data) # Get data from ESC
+                msg = esc_test_data_msg()
+                msg.escNum  = lastEsc_ # Add ESC Number
+                msg.temperature = data[0] # Add temperature data (deg C)
+                msg.voltage = data[1] / 255 + 10 # Add voltage data (V)
+                msg.current = (data[2] << 8 + data[3]) / 100 # Add current data (A)
+                msg.energy = (data[4] << 8 + data[5]) # Add energy data (mAHr)
+                msg.speed = (data[6] << 8 + data[7]) * 100 # Add speed data (erpm)
 
-                if (0x301 <= can_id <= 0x303): 
-                    msg = esc_test_data_msg()
-                    msg.escNum  = lastEsc[can_id - 0x301] # Add ESC Number
-                    data = list(can_rx.data) # Get data from ESC
-                    msg.temperature = data[0]
-                    msg.voltage = data[1] / 255 + 10
-                    msg.current = (data[2] << 8 + data[3]) / 100
-                    msg.energy = (data[4] << 8 + data[5])
-                    msg.speed = (data[6] << 8 + data[7]) * 100
-
-                    # Send information from node
-                    rospy.loginfo(msg)
-                    pub.publish(msg)
-                    rate.sleep()
+                # Send information from node
+                rospy.loginfo(msg)
+                pub.publish(msg)
+                rate.sleep()
 
 def main(args: list) -> None:
     """"""

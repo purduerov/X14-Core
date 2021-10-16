c77214a910503ac960431c40e90e160cefbb7764
babelman20 <babelman@purdue.edu>
2021-10-16 14:01:47 -0400

    Changed run order and added redundancy for rospy shutdown checks

--- ros/src/utils/src/ESC_Status.py
+++ ros/src/utils/src/ESC_Status.py
@@ -9,37 +9,40 @@ from shared_msgs.msg import esc_test_data_msg
 
 lastEsc = [0] * 3
 
-def sendEscData(can_rx: "can.Message") -> None:
-    pub = rospy.Publisher('ESCdata', esc_test_data_msg, queue_size=10) # Modify queue size based on rate
-    rospy.init_node('ESC_Logger', anonymous=True)
-    rate = rospy.Rate(10) # Figure out what rate to use
-    
-    can_id = can_rx.arbitration_id
-    if (0x301 <= can_id <= 0x303): 
-         msg = esc_test_data_msg()
-         msg.escNum  = lastEsc[can_id - 0x301] # Add ESC Number
-         data = list(can_rx.data) # Get data from ESC
-         msg.temperature = data[0]
-         msg.voltage = data[1] / 255 + 10
-         msg.current = (data[2] << 8 + data[3]) / 100
-         msg.energy = (data[4] << 8 + data[5])
-         msg.speed = (data[6] << 8 + data[7]) * 100
-         
-         # Send information from node
-         rospy.loginfo(msg)
-         pub.publish(msg)
-         rate.sleep()
+def sendEscData(can_bus):
+
+    try:
+        pub = rospy.Publisher('ESCdata', esc_test_data_msg, queue_size=10) # Modify queue size based on rate
+        rospy.init_node('ESC_Logger', anonymous=True)
+        rate = rospy.Rate(10) # Figure out what rate to use
+        
+        while not rospy.is_shutdown():
+            for can_rx in can_bus:
+                if not rospy.is_shutdown():
+                    can_id = can_rx.arbitration_id
+
+                    if (0x301 <= can_id <= 0x303): 
+                        msg = esc_test_data_msg()
+                        msg.escNum  = lastEsc[can_id - 0x301] # Add ESC Number
+                        data = list(can_rx.data) # Get data from ESC
+                        msg.temperature = data[0]
+                        msg.voltage = data[1] / 255 + 10
+                        msg.current = (data[2] << 8 + data[3]) / 100
+                        msg.energy = (data[4] << 8 + data[5])
+                        msg.speed = (data[6] << 8 + data[7]) * 100
+
+                        # Send information from node
+                        rospy.loginfo(msg)
+                        pub.publish(msg)
+                        rate.sleep()
+    except rospy.ROSInterruptException:
+	    pass
 
 def main(args: list) -> None:
     """"""
     channel = "can0"
     can_bus = can.interface.Bus(channel = channel, bustype = 'socketcan')
-
-    for can_rx in can_bus:
-        try:
-            sendEscData(can_rx)
-        except rospy.ROSInterruptException:
-            pass
+    sendEscData(can_bus)
 
 if(__name__ == "__main__"):
     main(sys.argv)
